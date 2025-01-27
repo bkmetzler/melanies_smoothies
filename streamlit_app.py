adcf829348344e278b0b874cb73a54b6832c1456
Brian Metzler <baldie@gmail.com>
2025-01-27 16:13:52 -0600

    Fixed search query

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ if ingredients_list:
         st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
 
         st.subheader(fruit_chosen + ' Nutrition Information')
-        url = f"https://my.smoothiefroot.com/api/fruit/{fruit_chosen}"
+        url = f"https://my.smoothiefroot.com/api/fruit/{search_on}"
         smoothiefroot_response = requests.get(url)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     
